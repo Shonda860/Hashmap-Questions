08421d9ea58505331c94fe88bcbc9d2fda09e08d
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-03-27 18:10:49 -0700

    completed insterect

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,3 +1,16 @@
 def intersection(list1, list2)
-  raise NotImplementedError, "Intersection not implemented"
+  # raise NotImplementedError, "Intersection not implemented"
+  hash = {}
+  list2.each do |num|
+    hash[num] = true
+  end
+
+  intersect_list = []
+
+  list1.each do |target|
+    if hash[target]
+      intersect_list << target
+    end
+  end
+  return intersect_list
 end
